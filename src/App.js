80d2db89a6dba8e4a0257e3f62c428f642ffebcf
krunal-m-purohit <krunal.m.purohit@pwc.com>
2018-08-28 17:45:42 +0530

    -ve values.

--- src/App.js
+++ src/App.js
@@ -38,56 +38,72 @@ class App extends Component {
 
     var g = svg.append('g')
       .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
-    
+
     d3.json('data.json').then(data => {
       var x = d3.scaleBand()
-          .domain(data.map(item => {
-            return item.point;
-          }))
-          .range([0, width])
-          .paddingInner(0.3)
-          .paddingOuter(0.3);
-  
+        .domain(data.map(item => {
+          return item.point;
+        }))
+        .range([0, width])
+        .paddingInner(0.3)
+        .paddingOuter(0.3);
+
       var y = d3.scaleLinear()
-          .domain([0, d3.max(data, (item) => {
-            return item.rallylength;
-          })])
-          .range([height, 0]);
+        .domain(d3.extent(data, (item) => { return item.rallylength })).nice();
+      //   [0, d3.max(data, (item) => {
+      //   return item.rallylength;
+      // })]
+      // )
+      y.range([height, 0]);
 
       var xAxisCall = d3.axisBottom(x);
+      g.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', 'translate(0, ' + y(0) + ')')
+        .call(xAxisCall)
+        .selectAll('text')
+        .attr('y', (item) => {
+          if (data[item - 1].rallylength > 0) {
+            return 15;
+          }
+          return -15;
+        })
+        .attr('class', 'x-labels');
+
+      var yAxisCall = d3.axisLeft(y)
+        .tickFormat((item) => {
+          if (item >= 0) {
+            return item;
+          }
+          return item * -1;
+        });
 
       g.append('g')
-          .attr('class', 'x axis')
-          .attr('transform', 'translate(0, ' + height + ')')
-          .call(xAxisCall)
-          .selectAll('text')
-            .attr('y', -15)
-            .attr('class', 'x-labels');
-      
-      var yAxisCall = d3.axisLeft(y);
+        .attr('class', 'y-axis')
+        .call(yAxisCall);
 
-      g.append('g')
-          .attr('class', 'y-axis')
-          .call(yAxisCall);
-  
       var rects = g.selectAll('rect')
-          .data(data)
-          .enter()
-          .append('rect')
-          .attr('x', (d, i) => {
-              return x(d.point);
-          })
-          .attr('y', (d) => {
+        .data(data)
+        .enter()
+        .append('rect')
+        .attr('x', (d, i) => {
+          return x(d.point);
+        })
+        .attr('y', (d) => {
+          if (d.rallylength > 0) {
             return y(d.rallylength);
-          })
-          .attr('height', (d) => {
-              return height - y(d.rallylength);
-          })
-          .attr('width', x.bandwidth)
-          // .attr('fill', 'grey');
-          .attr('fill', 'transparent');
+          }
+          return y(0);
+        })
+        .attr('height', (d) => {
+          return Math.abs(y(d.rallylength) - y(0));
+          // return height - y(d.rallylength);
+        })
+        .attr('width', x.bandwidth)
+        .attr('fill', 'grey');
+      // .attr('fill', 'transparent');
     }).catch(error => {
-        console.log(error);
+      console.log(error);
     });
   }
 }
